92e84865aafb95fbbc7e142af7262ba47db93209
Jan Komarek <jan.komarek@satoshilabs.com>
2023-05-03 11:54:23 +0200

    update testing gitbook revision [DO NOT MERGE]

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = 'cb339d12659cc03427f0499456e216fb89d022ec';
+export const GITBOOK_REVISION = '8bb914afa999f83c9bdf6c738df0c753abd7c01b';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const GITBOOK_ASSETS_DIR_PREFIX = '.gitbook';
