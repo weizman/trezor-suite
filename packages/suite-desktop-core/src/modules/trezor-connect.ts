d19c41650b85d2ba207048e5589d3ef763474329
Szymon Lesisz <sz.lesisz@gmail.com>
2023-11-01 11:07:22 +0100

    WIP: bl in suite-desktop

--- packages/suite-desktop-core/src/modules/trezor-connect.ts
+++ packages/suite-desktop-core/src/modules/trezor-connect.ts
@@ -3,11 +3,16 @@ import { app, ipcMain } from 'electron';
 import TrezorConnect from '@trezor/connect';
 import { createIpcProxyHandler, IpcProxyHandlerOptions } from '@trezor/ipc-proxy';
 
+import { WebBluetoothTransport } from './bluetooth';
+
 import type { Module } from './index';
+
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
+const protobufMessages = require('@trezor/protobuf/messages.json');
 
 export const SERVICE_NAME = '@trezor/connect';
 
-export const init: Module = ({ store }) => {
+export const init: Module = ({ store, mainWindow }) => {
     const { logger } = global;
     logger.info(SERVICE_NAME, `Starting service`);
 
@@ -24,6 +29,13 @@ export const init: Module = ({ store }) => {
             onRequest: async (method, params) => {
                 logger.debug(SERVICE_NAME, `call ${method}`);
                 if (method === 'init') {
+                    // if (params.transports?.includes('Web'))
+                    const BTT = new WebBluetoothTransport({
+                        mainWindow,
+                    });
+                    // @ts-expect-error TODO?
+                    params[0].transports = [BTT];
+
                     const response = await TrezorConnect[method](...params);
                     await setProxy(true);
                     return response;
