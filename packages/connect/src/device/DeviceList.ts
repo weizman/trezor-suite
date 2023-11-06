52f4400358745d69cc76cf86e995bf83600a2727
Szymon Lesisz <sz.lesisz@gmail.com>
2023-11-06 13:49:32 +0100

    fix(connect): check custom Transport instance
    `instanceof Transport` is not always working - especially when transport package and connect package are bundled in separate chunks

--- packages/connect/src/device/DeviceList.ts
+++ packages/connect/src/device/DeviceList.ts
@@ -13,6 +13,7 @@ import {
     TRANSPORT,
     Descriptor,
     TRANSPORT_ERROR,
+    isTransportInstance,
 } from '@trezor/transport';
 import { ERRORS } from '../constants';
 import { DEVICE, TransportInfo } from '../events';
@@ -131,7 +132,7 @@ export class DeviceList extends TypedEmitter<DeviceListEvents> {
                             `DeviceList.init: transports[] of unexpected type: ${transportType}`,
                         );
                 }
-            } else if (transportType instanceof Transport) {
+            } else if (isTransportInstance(transportType)) {
                 // custom Transport might be initialized without messages, update them if so
                 if (!transportType.getMessage()) {
                     transportType.updateMessages(this.messages);
