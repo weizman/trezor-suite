99f7ec3570e484b4f3fcb5169c8cc4a8ce60b452
Samuel Šulovský <samuel.sulovsky@vacuumlabs.com>
2023-12-04 12:17:42 +0100

    fix(suite-native): display 'receive address' istead of 'XPUB' for SOL

--- suite-native/qr-code/src/components/XpubQRCodeBottomSheet.tsx
+++ suite-native/qr-code/src/components/XpubQRCodeBottomSheet.tsx
@@ -18,7 +18,7 @@ const networkTypeToSheetTitleMap: Record<NetworkType, string> = {
     cardano: 'Public key (XPUB)',
     ethereum: 'Receive address',
     ripple: 'Receive address',
-    solana: 'Public key (XPUB)',
+    solana: 'Receive address',
 };
 
 const buttonStyle = prepareNativeStyle(utils => ({
