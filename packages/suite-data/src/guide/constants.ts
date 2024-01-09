0ff89c18be9cfc160edb8b4d84c590b93a3cf849
trezor-ci <trezor-ci@trezor.io>
2024-01-09 14:20:16 +0000

    chore(suite-data): update guide revision

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = 'beb89c7131a2bea9e417fb77eb74cf3dda39f0b3';
+export const GITBOOK_REVISION = 'e7190508871d76788fe9adbba96c965ce7001957';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const GITBOOK_ASSETS_DIR_PREFIX = '.gitbook';
